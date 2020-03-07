34a6161de96b92f5f7485e05edb4a8b8af798b27
Yanglin Zhao <yz1876@gmail.com>
2020-03-07 16:28:26 -0500

    Reduce the length of berglas secrets store by removing the backpack prefix

--- .backpack/terraform/web-django-module/main.tf
+++ .backpack/terraform/web-django-module/main.tf
@@ -1,6 +1,6 @@
 locals {
   docker_image        = "gcr.io/${var.gcp_project}/${var.context_name}:${var.image_tag}"
-  berglas_secret_path = "backpack-berglas-${var.context_name}/BERGLAS_APP_JSON"
+  berglas_secret_path = "berglas-${var.context_name}/BERGLAS_APP_JSON"
   service_account     = "cloudrun-berglas-${var.context_name}@${var.gcp_project}.iam.gserviceaccount.com"
 }
 
