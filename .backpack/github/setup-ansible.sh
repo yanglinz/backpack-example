1b578b35d4ae461bfcf1bbd9e05618c5a1027101
Yanglin Zhao <yz1876@gmail.com>
2020-09-19 22:13:39 -0400

    Move logic to a bash file

---
+++ .backpack/github/setup-ansible.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# Install ansible
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt update && sudo apt install ansible
+
+instance_ip=$(cat /tmp/terraform-output.json | jq)
+
+# Setup ansible hosts inventory
+mkdir -p ~/.ssh
+mkdir -p ./etc/ansible
+ssh-keyscan -H "$instance_ip" >> ~/.ssh/known_hosts
+echo "$instance_ip" > ./etc/ansible/hosts
+
+# Setup SSH key pair
+mkdir -p ./etc/ssh
+echo "$DIGITALOCEAN_PRIVATE_KEY" > ./etc/ssh/id_rsa
+ssh-keygen -y -f ./etc/ssh/id_rsa > ./etc/ssh/id_rsa.pub
+sudo chmod 600 ./etc/ssh/id_rsa.pub
+sudo chmod 600 ./etc/ssh/id_rsa
