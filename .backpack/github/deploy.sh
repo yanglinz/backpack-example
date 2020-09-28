2e8f80c509303a3941db6a893571d9d7452ddcb0
Yanglin Zhao <yz1876@gmail.com>
2020-09-27 22:02:03 -0400

    Add some debug info

--- .backpack/github/deploy.sh
+++ .backpack/github/deploy.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 instance_ip=$(cat /tmp/terraform-output.json | jq -r '.ip_address.value')
+local_branch=$(git rev-parse --abbrev-ref HEAD)
 
 # Setup ansible hosts inventory
 mkdir -p ~/.ssh
@@ -15,6 +16,9 @@ sudo chmod 600 ~/.ssh/id_rsa
 ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub
 sudo chmod 600 ~/.ssh/id_rsa.pub
 
+echo "Debug: ${instance_ip}"
+echo "Debug: ${local_branch}"
+
 # Setup git remote and push remote
 git remote add dokku dokku@"$instance_ip":"$APP_NAME"
-GIT_SSH_COMMAND="ssh -l dokku" git push dokku "${GITHUB_REF##*/}":master -f
+GIT_SSH_COMMAND="ssh -l dokku" git push dokku "$local_branch":master -f
