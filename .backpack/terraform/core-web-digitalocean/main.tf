6ed9ac60a1d3ed67709a1199206a642cea5ab86c
Yanglin Zhao <yz1876@gmail.com>
2020-10-02 10:03:05 -0400

    Setup firewall rules for droplet

--- .backpack/terraform/core-web-digitalocean/main.tf
+++ .backpack/terraform/core-web-digitalocean/main.tf
@@ -1,6 +1,6 @@
 locals {
-  # We can and should set this dynamically by fetching it from 
-  # the digitalocean api, but this will do for now
+  # Ideally we should set the key id dynamically by fetching it from the DO 
+  # API, but since it changes so infrequently, hardcoding it is fine for now.
   default_ssh_key_ids = ["27895764"]
 }
 
@@ -15,6 +15,52 @@ resource "digitalocean_droplet" "web" {
 resource "digitalocean_floating_ip" "web" {
   droplet_id = digitalocean_droplet.web.id
   region     = digitalocean_droplet.web.region
+}
+
+resource "digitalocean_firewall" "web" {
+  name = "backpack-web-firewall"
+
+  droplet_ids = [digitalocean_droplet.web.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["192.168.1.0/24", "2002:1:2::/48"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "icmp"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
 }
 
 output "ip_address" {
