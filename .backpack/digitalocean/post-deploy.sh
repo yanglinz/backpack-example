d7b68e69ab1fee70a114ae052b5e2c03851480ee
Yanglin Zhao <yanglinz@users.noreply.github.com>
2020-10-01 23:13:28 -0400

    Setup variables and secrets in dokku (#19)
    * Testing
    
    * More push
    
    * Run post-deployment
    
    * We need terraform apply
    
    * Base64 encode json
    
    * Run post-deployment
    
    * Env is production
    
    * Environment
    
    * Config set
    
    * Add APP_NAME
    
    * Use the correct ENV_SOURCE path
    
    * Deploy
    
    * Remove trailing whitespace

---
+++ .backpack/digitalocean/post-deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+ENV_SOURCE="/opt/backpack-app/var/env/production.json"
+
+echo "Setting ${APP_NAME} application configs..."
+
+for name in $(jq --raw-output 'keys | .[]' "$ENV_SOURCE"); do
+  value=$(jq --raw-output ".${name}" "$ENV_SOURCE")
+  dokku config:set --no-restart "$APP_NAME" "$name"="$value"
+done
+
+dokku config:set "$APP_NAME" TIMESTAMP=$(date +%s)
