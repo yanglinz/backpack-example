01b75a360911193b3799dc29a84edaf4a9f62a26
Yanglin Zhao <yz1876@gmail.com>
2020-10-02 10:32:30 -0400

    Allow a wider range of inbound ips in port 22

--- .backpack/terraform/core-web-digitalocean/main.tf
+++ .backpack/terraform/core-web-digitalocean/main.tf
@@ -25,7 +25,7 @@ resource "digitalocean_firewall" "web" {
   inbound_rule {
     protocol         = "tcp"
     port_range       = "22"
-    source_addresses = ["192.168.1.0/24", "2002:1:2::/48"]
+    source_addresses = ["0.0.0.0/0", "::/0"]
   }
 
   inbound_rule {
